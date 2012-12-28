076ac9bb6f13e3be3a46a395803ad7338e816b4c
kofis <k0fis@me.com>
2012-12-28 10:33:58 +0100

    add task pojo class

--- src/kfs/kfsWfl/wflTask.java
+++ src/kfs/kfsWfl/wflTask.java
@@ -85,20 +85,82 @@ public class wflTask extends kfsDbObject {
     public void setArchivedDate(kfsRowData r, Date lch) {
         archivedDate.setDate(lch, r);
     }
-    
+
     public String sqlGetTaskById() {
-        return getSelect(getName(), getColumns(), new kfsDbiColumn[] {id});
+        return getSelect(getName(), getColumns(), new kfsDbiColumn[]{id});
     }
-    
+
     public void psGetTaskById(PreparedStatement ps, int taskId) throws SQLException {
         ps.setInt(1, taskId);
     }
-    
+
     public String getOwnerLogin(kfsRowData r) {
         return this.ownerLogin.getString(r);
     }
-    
+
     public void setOwnerLogin(kfsRowData r, String ownerLogin) {
         this.ownerLogin.setString(ownerLogin, r);
+    }
+
+    @Override
+    public pojo getPojo(kfsRowData row) {
+        return new pojo(row);
+    }
+
+    
+    public class pojo extends kfsPojoObj<wflTask> {
+
+        public pojo(kfsRowData rd) {
+            super(wflTask.this, rd);
+        }
+
+        public int getId() {
+            return id.getInt(rd);
+        }
+
+        public Integer getTemplateId() {
+            return idTemplate.getInt(rd);
+        }
+
+        public String getName() {
+            return name.getString(rd);
+        }
+
+        public void setName(String newName) {
+            name.setString(newName, rd);
+        }
+
+        public int getFirstNodeId() {
+            return firstNodeId.getInt(rd);
+        }
+
+        public void setFirstNodeId(int fist) {
+            firstNodeId.setInt(fist, rd);
+        }
+
+        public Date getLastChange() {
+            return lastChange.getDate(rd);
+        }
+
+        public void setLastChange( Date lch) {
+            lastChange.setDate(lch, rd);
+        }
+
+        public Date getArchivedDate() {
+            return archivedDate.getDate(rd);
+        }
+
+        public void setArchivedDate(Date lch) {
+            archivedDate.setDate(lch, rd);
+        }
+
+
+        public String getOwnerLogin() {
+            return ownerLogin.getString(rd);
+        }
+
+        public void setOwnerLogin(String owner) {
+            ownerLogin.setString(owner, rd);
+        }
     }
 }
