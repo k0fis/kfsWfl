1343ee8fefd8afd6af50f3d282cca08ad464b203
kofis <k0fis@me.com>
2012-12-29 00:37:44 +0100

    create snapshot script

--- snapshot.sh
+++ snapshot.sh
@@ -1,1 +1,6 @@
 mvn -DaltDeploymentRepository=snapshot-repo::default::file:../kfsWfl-mvn/snapshots/ clean deploy
+PATH=/usr/local/git/bin
+cd ../kfsWfl-mvn
+git add *
+git commit -m 'snapshot'
+git push origin master
