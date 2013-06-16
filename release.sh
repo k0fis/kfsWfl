c6e687a289aa1b41a5461c6a9fa22053a89bd5a1
kofis <k0fis@me.com>
2013-06-16 08:42:52 +0200

    add git pull into create release and snapshot.

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 PATH=/bin:/usr/bin:/usr/local/git/bin
 mvn -DaltDeploymentRepository=repo::default::file:../kfsWfl-mvn/release clean deploy 
 cd ../kfsWfl-mvn
+git pull
 git add *
 git commit -m 'release'
 git push origin master
