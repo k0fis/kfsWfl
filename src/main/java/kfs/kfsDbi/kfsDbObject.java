d4b356916549d2b8b905efe26cfc5cbbc8514b82
kofis <k0fis@me.com>
2013-12-17 19:02:40 +0100

    Create export into CSV snd CTL file for custom columns

--- src/main/java/kfs/kfsDbi/kfsDbObject.java
+++ src/main/java/kfs/kfsDbi/kfsDbObject.java
@@ -75,13 +75,16 @@ public class kfsDbObject implements kfsDbiTable, kfsTableDesc, Comparator<kfsRow
     }
 
     public String getOracleLoaderControlFile() {
+        return getOracleLoaderControlFile(allCols);
+    }
+    public String getOracleLoaderControlFile(kfsDbiColumn []cols) {
         StringBuilder sb = new StringBuilder();
         sb//
                 .append("load data append into table ")//
                 .append(getName())//
                 .append(" fields terminated by \",\" optionally enclosed by '\"' TRAILING NULLCOLS (");
         boolean f = true;
-        for (kfsDbiColumn di : allCols) {
+        for (kfsDbiColumn di : cols) {
             if (f) {
                 f = false;
             } else {
